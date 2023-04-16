23dc0ff659e67830f1089a467c3bfbd67476da0d
Валиахмедов Бари <valiakhmedov.ba@gmail.com>
2023-04-16 20:13:07 +0300

    init-5

--- src/test/java/hexlet/code/app/AppApplicationTests.java
+++ src/test/java/hexlet/code/app/AppApplicationTests.java
@@ -1,14 +1,15 @@
 package hexlet.code.app;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 class AppApplicationTests {
 
-	@Test
-	void testInit() {
-		assertThat(true).isTrue();
-	}
+    @Test
+    void testInit() {
+        assertThat(true).isTrue();
+    }
 
 }
