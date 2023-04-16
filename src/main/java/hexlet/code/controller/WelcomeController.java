c05ce7bcf7a4ad513fe36dd4ef865a89aa43504b
Валиахмедов Бари <valiakhmedov.ba@gmail.com>
2023-04-16 19:38:00 +0300

    init-4

--- src/main/java/hexlet/code/controller/WelcomeController.java
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package hexlet.code.app.controller;
+package hexlet.code.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
