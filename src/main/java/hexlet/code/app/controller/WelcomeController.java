5f92fcf00d1d8c8c98909ffb4bcba3f54a95228e
Валиахмедов Бари <valiakhmedov.ba@gmail.com>
2023-04-16 19:14:25 +0300

    init

---
+++ src/main/java/hexlet/code/app/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package hexlet.code.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeController {
+    @GetMapping
+    public String welcome() {
+        return "Welcome to Spring";
+    }
+}
