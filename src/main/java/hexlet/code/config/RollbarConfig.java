09f2547b87c61f47cd9da555709191f31be61935
Валиахмедов Бари <BValiakhmedov@alfabank.ru>
2023-05-17 23:20:14 +0300

    add ROLLBAR_TOKEN, complete step 11

---
+++ src/main/java/hexlet/code/config/RollbarConfig.java
@@ -0,0 +1,33 @@
+package hexlet.code.config;
+
+import com.rollbar.notifier.Rollbar;
+import com.rollbar.notifier.config.Config;
+import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration()
+@ComponentScan({"hexlet.code"})
+public class RollbarConfig {
+
+    @Value("${rollbar_token:}")
+    private String rollbarToken;
+
+    @Value("${spring.profiles.active:}")
+    private String activeProfile;
+
+    @Bean
+    public Rollbar rollbar() {
+        return new Rollbar(getRollbarConfigs(rollbarToken));
+    }
+
+    private Config getRollbarConfigs(String accessToken) {
+
+        // Reference ConfigBuilder.java for all the properties you can set for Rollbar
+        return RollbarSpringConfigBuilder.withAccessToken(accessToken)
+                .environment(activeProfile)
+                .build();
+    }
+}
