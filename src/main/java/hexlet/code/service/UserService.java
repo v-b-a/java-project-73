80f4ac5803d27c8f92c69d8855e2746c903630f6
Валиахмедов Бари <BValiakhmedov@alfabank.ru>
2023-06-19 22:01:53 +0300

    update

--- src/main/java/hexlet/code/service/UserService.java
+++ src/main/java/hexlet/code/service/UserService.java
@@ -1,11 +1,12 @@
 package hexlet.code.service;
 
-import hexlet.code.repository.model.User;
-import hexlet.code.repository.UserRepository;
 import hexlet.code.dto.UserDtoRq;
 import hexlet.code.dto.UserDtoRs;
+import hexlet.code.repository.UserRepository;
+import hexlet.code.repository.model.User;
 import hexlet.code.service.mapper.UserMapper;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,8 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-import static hexlet.code.config.SecurityConfig.DEFAULT_AUTHORITIES;
 
 @Service
 @AllArgsConstructor
@@ -54,15 +53,14 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username)
-                .map(this::buildSpringUser)
+                .map(this::toSpringUser)
                 .orElseThrow(() -> new UsernameNotFoundException("Not found user with 'username': " + username));
     }
 
-    private UserDetails buildSpringUser(final User user) {
+    private org.springframework.security.core.userdetails.User toSpringUser(final User user) {
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(),
                 user.getPassword(),
-                DEFAULT_AUTHORITIES
-        );
+                List.of(new SimpleGrantedAuthority("USER")));
     }
 }
