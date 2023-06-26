38aaa0fd068d154e4afb6468dac423ec44920d3b
Валиахмедов Бари <BValiakhmedov@alfabank.ru>
2023-06-26 20:51:01 +0300

    update12

--- src/main/java/hexlet/code/controller/UserController.java
+++ src/main/java/hexlet/code/controller/UserController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -49,6 +51,7 @@ public class UserController {
     }
 
     @Operation(summary = "Create user")
+    @ResponseStatus(HttpStatus.CREATED)
     @ApiResponse(responseCode = "201", description = "User created")
     @PostMapping("")
     public UserDtoRs createUser(@RequestBody @Valid UserDtoRq user) {
